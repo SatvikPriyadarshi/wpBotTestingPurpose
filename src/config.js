f4b1a3bc2f3291f1b3a7ff47087d7a01d9399362
SatvikPriyadarshi <158612373+SatvikPriyadarshi@users.noreply.github.com>
2026-04-02 19:36:28 +0530

    Fix: Add production-ready Puppeteer args for headless servers

--- src/config.js
+++ src/config.js
@@ -48,7 +48,16 @@ module.exports = {
         // Puppeteer options
         puppeteer: {
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process',
+                '--disable-gpu'
+            ]
         }
     },
     
